235bb77fa666b52a764642aea8947c3bd6e6938e
katraoui <katraoui.youness@gmail.com>
2020-03-06 19:43:08 +0100

    added logrus

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/katraoui/yocto-embedded-server
 
 go 1.12
 
+require github.com/sirupsen/logrus v1.4.2 // indirect
--- websvr.go
+++ websvr.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	fmt.Println("!selpmaxe oG ,olleH")
+	log.Info("tadaaaa!")
 
 }
